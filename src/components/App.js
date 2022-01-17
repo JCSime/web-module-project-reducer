ceb7ebc3343db4a3d4ca63d3d34cc7d33d3cf46b
John S <johnc.sime@gmail.com>
2022-01-17 16:22:10 -0500

    connected the clear display button

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
 
 import reducer, { initialState } from '../reducers';
-import { applyNumber, changeOperation } from '../actions';
+import { applyNumber, changeOperation, clearDisplay } from '../actions';
 
 function App() {
   const [ state, dispatch] = useReducer(reducer, initialState);
@@ -16,6 +16,9 @@ function App() {
   }
   const handleOperationClick = (operator) => {
     dispatch(changeOperation(operator));
+  }
+  const handleClearDisplay = () => {
+    dispatch(clearDisplay());
   }
 
   return (
@@ -65,7 +68,7 @@ function App() {
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton value={"CE"} onClick={handleClearDisplay}/>
             </div>
 
           </form>
